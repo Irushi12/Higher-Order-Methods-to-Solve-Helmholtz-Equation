15b94170c4e66691740534a2fbfbef26259b9573
Irushi12 <162843249+Irushi12@users.noreply.github.com>
2025-05-21 14:50:29 -0600

    Update ccm_6_bc4_dm.m

--- 1D Problem/ccm_6_bc4_dm.m
+++ 1D Problem/ccm_6_bc4_dm.m
@@ -53,7 +53,7 @@ A(N+1,N+1) = 1; A(N+2,N+2) = 1;
 B = P*F-R; 
 B(2) = u1; B(N+1) = uend;
 %Additional boundary from fourth order solution
-u4=ccm_4_dm(u1,uend,f,k,N);
+u4=cm_4_dm(u1,uend,f,k,N);
 B(1) = 7*u4(1) - 21*u4(2) + 35*u4(3) - 35*u4(4) + 21*u4(5) -7*u4(6) +u4(7);
 B(end) = 7*u4(end) - 21*u4(end-1) + 35*u4(end-2) - 35*u4(end-3) + 21*u4(end-4) -7*u4(end-5) +u4(end-6);
 u=A\B;u= u(2:end-1);
